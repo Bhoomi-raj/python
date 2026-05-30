885fedf06f38401b9a7cb88d80637c4e226abc68
Bhoomi-raj <bhoomiraj2006@gmail.com>
2026-05-30 19:12:05 +0530

    new lib

--- matplotlib_prg_file.py/bars.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-xp = np.array(['A','B','C','D'])
-yp = np.array([1,2,3,4])
-#plt.bar(xp,yp)
-###plt horizontal bar graph
-plt.barh(xp,yp)
-
-plt.show()
